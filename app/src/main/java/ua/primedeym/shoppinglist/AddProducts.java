bb9a1479a142f07fc032e69c089b80b82156e5aa
Slava Kostuchenko <primedeym@gmail.com>
2016-08-30 12:13:10 +0300

    DB and AddProduct activity

--- app/src/main/java/ua/primedeym/shoppinglist/AddProducts.java
+++ app/src/main/java/ua/primedeym/shoppinglist/AddProducts.java
@@ -1,13 +1,45 @@
 package ua.primedeym.shoppinglist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddProducts extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    EditText editText;
+    SLDatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_products);
+        editText = (EditText) findViewById(R.id.editText);
+        databaseHelper = new SLDatabaseHelper(this);
+
+    }
+
+    public void addProductsToList(View view) {
+        String text = editText.getText().toString();
+        try {
+            databaseHelper.insertProduct(text, "magazine");
+            Toast.makeText(AddProducts.this, "Вы успешно добавили товар в список покупок", Toast.LENGTH_SHORT).show();
+        } catch (SQLException e) {
+            Toast.makeText(AddProducts.this, "База данных не доступна", Toast.LENGTH_SHORT).show();
+        }
+        editText.setText(" ");
+    }
+
+    public void addProductToFavorite(View view) {
+        String text = editText.getText().toString();
+        try {
+            Toast.makeText(AddProducts.this, "Товар добавлен в избранное", Toast.LENGTH_SHORT).show();
+        } catch (SQLException e) {
+            Toast.makeText(AddProducts.this, "База данных не доступна", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/ua/primedeym/shoppinglist/SLDatabaseHelper.java
+++ app/src/main/java/ua/primedeym/shoppinglist/SLDatabaseHelper.java
@@ -8,12 +8,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SLDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "shoppingListDB";
-    public static final int DB_VERSION = 1;
+    public static final int DB_VERSION = 3;
 
     public static final String TABLE_NAME = "Products";
     public static final String COL_NAME = "NAME";
     public static final String COL_MAGAZINE = "MAGAZINE";
     public static final String COL_BOUGHT = "NO";
+    public static final String COL_FAVORITE = "NO";
 
     public SLDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -21,9 +22,10 @@ public class SLDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String query = "create table Products (_id PRIMARY INTEGER AUTOINCREMENT, "
+        String query = "create table " + TABLE_NAME + "(_id integer primary key autoincrement, "
                 + COL_NAME + " TEXT, "
                 + COL_MAGAZINE + " TEXT, "
+                + COL_FAVORITE + " TEXT, "
                 + COL_BOUGHT + " TEXT);";
         sqLiteDatabase.execSQL(query);
     }
@@ -35,19 +37,25 @@ public class SLDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public void insertProduct(String name, String magazine) {
-        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_NAME, name);
         contentValues.put(COL_MAGAZINE, magazine);
         contentValues.put(COL_BOUGHT, "NO");
+        contentValues.put(COL_FAVORITE, "NO");
         //Create check for true or false
-        long rowIns = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+        db.insert(TABLE_NAME, null, contentValues);
+        db.close();
     }
 
     public void updateStatus(long rowID){
-        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_BOUGHT, "YES");
         sqLiteDatabase.update(TABLE_NAME, contentValues, "_id = ?", new String[] {String.valueOf(rowID)});
+    }
+
+    public void updateFavoriteProduct(){
+
     }
 }
