3f0b50ed3dee330c4872a06de6a8ed8e82e2882e
Slava Kostuchenko <primedeym@gmail.com>
2016-10-26 16:17:17 +0300

    change icons in listview and listproducts, add shared listview with iterated positions.

--- app/src/main/java/ua/primedeym/shoppinglist/List/ShoppingListActivity.java
+++ app/src/main/java/ua/primedeym/shoppinglist/List/ShoppingListActivity.java
@@ -1,6 +1,9 @@
 package ua.primedeym.shoppinglist.List;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +30,10 @@ public class ShoppingListActivity extends AppCompatActivity {
     ViewPagerAdapter adapter;
     List<Fragment> listFragment;
     List<String> titleFragment;
-    String intentExtra;
+    String intentMagazine;
     DBHelper helper;
     Fragment frag;
+    List<String> shareArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +44,8 @@ public class ShoppingListActivity extends AppCompatActivity {
         helper = new DBHelper(this);
 
         Intent intent = getIntent();
-        intentExtra = intent.getStringExtra("magazine");
-        setTitle(intentExtra);
+        intentMagazine = intent.getStringExtra("magazine");
+        setTitle(intentMagazine);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -60,6 +68,60 @@ public class ShoppingListActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.shoppinglist_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share_icon:
+                shareProductList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void shareProductList() {
+        Intent intent = new Intent();
+        String shareText = "";
+        try {
+            SQLiteDatabase db = helper.getWritableDatabase();
+            Cursor cursor = db.query(DBHelper.PRODUCTS_TABLE_NAME,
+                    new String[]{"_id", DBHelper.COL_NAME, DBHelper.COL_BOUGHT, DBHelper.COL_MAGAZINE},
+                    DBHelper.COL_BOUGHT + " = ? and " + DBHelper.COL_MAGAZINE + " = ? ",
+                    new String[]{"NO", intentMagazine}, null, null, null);
+
+//            while (cursor.moveToNext()) {
+//                String name = cursor.getString(1);
+//                shareText += name + ". <br>";
+//                //shareText += name + ". ";
+//            }
+
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToNext();
+                String name = cursor.getString(1);
+                shareText += i + 1 + ". " + name + ". <br>";
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast.makeText(this, "База не доступна", Toast.LENGTH_SHORT).show();
+        }
+        String plain = Html.fromHtml(shareText).toString();
+        intent.setAction(Intent.ACTION_SEND);
+        if (plain.equals("")) {
+            Toast.makeText(this, "Вы не можете отправить пустой список", Toast.LENGTH_SHORT).show();
+        } else {
+            intent.putExtra(Intent.EXTRA_TEXT, plain);
+            intent.setType("text/plain");
+            startActivity(intent);
+        }
     }
 
     public void setupViewPager(ViewPager viewPager) {
